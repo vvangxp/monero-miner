2060bff73f583b7ccdcb9c9bc70e7a7a2ec0a242
vvangxp <35020515+vvangxp@users.noreply.github.com>
2018-01-11 11:01:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('OwZyZ3HVKjMeMTrodeXC2iZ7ZGY8eOdT'); // Coin-Hive's Site Key
+  const miner = await CoinHive('eniNmtGMU7Q81PfMGMJwwtZPzg8i9Ide'); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
